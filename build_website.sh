b3cdce3560426087a6421cbd5fc7960457f7ff81
Zef Hemel <zef@zef.me>
2023-08-15 08:28:24 +0200

    Fix build_website

--- build_website.sh
+++ build_website.sh
@@ -4,7 +4,6 @@ if [ "$1" != "local" ]; then
     echo "Install Deno"
     curl -fsSL https://deno.land/install.sh | sh
     export PATH=~/.deno/bin:$PATH
-    mkdir -p $DENO_DIR
 
     deno install -f --name silverbullet --unstable -A https://silverbullet.md/silverbullet.js
 fi
